5b7da04251806964b69532ba8194e7617683e327
Mehmet Guler <mehmetgspace@gmail.com>
2020-05-09 05:00:46 +0300

    Add browser-sync package

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 function style() {
   return gulp.src('./app/assets/styles/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./app/assets/styles/'))
+    .pipe(browserSync.stream())
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    startPath: './app/index.html'
+  });
+  gulp.watch('./app/assets/styles/**/*.scss', style);
+  gulp.watch('./app/*.html').on('change', browserSync.reload);
+  gulp.watch('./app/assets/scripts/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
